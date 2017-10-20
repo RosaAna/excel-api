1b6b7410797d0151588d189c27c43fe4c292708e
Jose María Ruano Gálvez <jruanogalvez@gmail.com>
2017-10-20 12:38:45 +0200

    modified:   src/main/java/com/jruanogalvez/excelapi/Main.java 	modified:   target/classes/com/jruanogalvez/excelapi/Main.class
    	Terminado mÃtodo para leer archivos XSLX, comenzado mÃtodo para
    	escribir el Array obtenido.

--- src/main/java/com/jruanogalvez/excelapi/Main.java
+++ src/main/java/com/jruanogalvez/excelapi/Main.java
@@ -3,10 +3,11 @@ package com.jruanogalvez.excelapi;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
@@ -22,9 +23,6 @@ public class Main {
     public static ArrayList<ArrayList<String>> transformToArray(String inputFile) {
         FileInputStream input;
         XSSFWorkbook wb;
-                
-        int rowCount = 0;
-        int cellCount = 0;
         
         ArrayList<ArrayList<String>> data = new ArrayList<>();
         ArrayList<String> rowList = new ArrayList<>();
@@ -35,25 +33,26 @@ public class Main {
             
             for(int sheetCount = 0; sheetCount < wb.getNumberOfSheets(); sheetCount++) {
                 Sheet firstSheet = wb.getSheetAt(0);
-                Iterator<Row> iterator = firstSheet.iterator();
                 
-                while(iterator.hasNext()) {
-                    
-                    Row thisRow = iterator.next();
-                    Iterator<Cell> cellIterator = thisRow.cellIterator();
-                    rowCount += 1;
-                    
-                    while(cellIterator.hasNext()) {
-                        Cell thisCell = cellIterator.next();
-                        cellCount += 1;
-                        
-                        if (thisCell.getCellTypeEnum() == CellType.STRING) {
-                            rowList.add(thisCell.getStringCellValue());
-                            
-                        } else if (thisCell.getCellTypeEnum() == CellType.NUMERIC) {
-                            rowList.add(Double.toString(thisCell.getNumericCellValue()));
+                for(int i = 0; i <= firstSheet.getLastRowNum(); i++) {
+                    Row thisRow = firstSheet.getRow(i);
+                  
+                    if(thisRow != null) {
+                        for(int j = 0; j <= thisRow.getLastCellNum(); j++) {
+                            Cell thisCell = thisRow.getCell(j);
+
+                            if(thisCell != null) {
+                                if(thisCell.getCellTypeEnum() == CellType.STRING)
+                                    rowList.add(thisCell.getStringCellValue());
+                                if(thisCell.getCellTypeEnum() == CellType.NUMERIC)
+                                    rowList.add(Double.toString(thisCell.getNumericCellValue()));
+                                if(thisCell.getCellTypeEnum() == CellType.BLANK)
+                                    rowList.add(" ");
+                            } else {
+                                rowList.add(" ");
+                            }
                         }
-                    }
+                    } 
                     
                     data.add((ArrayList<String>) rowList.clone());
                     rowList.clear();
@@ -72,15 +71,41 @@ public class Main {
             
         }
         
-        for (List<String> element : data) {
+        /*data.forEach((element) -> {
             System.out.println(element);
-        }
+        });*/
+        
+        transformToExcel(data, "C:\\Users\\matinal\\Downloads\\example2.xlsx");
         
         return data;
     }
-    
-    public void transformToExcel(ArrayList<ArrayList<String>> data) {
+
+    public static void transformToExcel(ArrayList<ArrayList<String>> data, String path) {
         
+        XSSFWorkbook wb = new XSSFWorkbook();
+        Sheet thisSheet = wb.createSheet();
+        
+        thisSheet.createRow(data.size());
+        
+        for(int i = 0; i < thisSheet.getLastRowNum(); i++) {
+            Row thisRow = thisSheet.getRow(i);
+            thisRow.createCell(data.get(i).size());
+            
+            for(int j = 0; j < thisRow.getLastCellNum(); j++) {
+                Cell thisCell = thisRow.getCell(j);
+                thisCell.setCellValue(data.get(i).get(j));
+            }
+        }
+        
+        try {
+            FileOutputStream out = new FileOutputStream(new File(path));
+            wb.write(out);
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se ha encontrado el archivo de salida.");
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
     }
