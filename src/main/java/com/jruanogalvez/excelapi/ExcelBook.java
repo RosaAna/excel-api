a9fb1e5dfe316717f9387f2a80888f4b2180a7bd
Jose María Ruano Gálvez <jruanogalvez@gmail.com>
2017-10-25 12:08:01 +0200

    Refactorized Main class splitting the readExcel method to one 	method in the ExcelBook class.

--- src/main/java/com/jruanogalvez/excelapi/ExcelBook.java
+++ src/main/java/com/jruanogalvez/excelapi/ExcelBook.java
@@ -1,12 +1,15 @@
 package com.jruanogalvez.excelapi;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -16,10 +19,15 @@ public class ExcelBook {
     private ArrayList<ArrayList<String>> data;
     private String path;
     private File output;
+    private String inputFile;
 
     public ExcelBook(ArrayList<ArrayList<String>> data, String path) {
         this.data = data;
         this.path = path;
+    }
+    
+    public ExcelBook(String inputFile) {
+        this.inputFile = inputFile;
     }
     
     public boolean checkPathSyntax() throws ExcelAPISyntaxException {
@@ -28,6 +36,64 @@ public class ExcelBook {
         else
             throw new ExcelAPISyntaxException("El nombre del archivo " + path +
                     " es incorrecto.");
+    }
+    
+    public ArrayList<ArrayList<String>> readExcelFile() {
+        FileInputStream input = null;
+        XSSFWorkbook wb = null;
+        
+        ArrayList<ArrayList<String>> data = new ArrayList<>();
+        ArrayList<String> rowList = new ArrayList<>();
+        
+        try {
+            input = new FileInputStream(new File(inputFile));
+            wb = new XSSFWorkbook(input);
+            
+            for(int sheetCount = 0; sheetCount < wb.getNumberOfSheets(); sheetCount++) {
+                Sheet firstSheet = wb.getSheetAt(0);
+                
+                for(int i = 0; i <= firstSheet.getLastRowNum(); i++) {
+                    Row thisRow = firstSheet.getRow(i);
+                  
+                    if(thisRow != null) {
+                        for(int j = 0; j <= thisRow.getLastCellNum(); j++) {
+                            Cell thisCell = thisRow.getCell(j);
+
+                            if(thisCell != null) {
+                                if(thisCell.getCellTypeEnum() == CellType.STRING)
+                                    rowList.add(thisCell.getStringCellValue());
+                                if(thisCell.getCellTypeEnum() == CellType.NUMERIC)
+                                    rowList.add(Double.toString(thisCell.getNumericCellValue()));
+                                if(thisCell.getCellTypeEnum() == CellType.BLANK)
+                                    rowList.add(" ");
+                            } else {
+                                rowList.add(" ");
+                            }
+                        }
+                    } 
+                    
+                    data.add((ArrayList<String>) rowList.clone());
+                    rowList.clear();
+                    
+                }
+            }
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se puede encontrar el archivo especificado.");
+            
+        } catch (IOException ex) {
+            System.out.println("No se puede leer o escribir el archivo.");
+            
+        } finally {
+            try {
+                wb.close();
+                input.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return data;
     }
     
     public void writeExcelSheet() throws ExcelAPISyntaxException {
